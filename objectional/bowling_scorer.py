c4eb69e5a19b65fca8f61e359ed0e33a44ba5bc8
seamus <house.alabama@gmail.com>
2017-04-22 11:14:24 -0400

    Write getters and setters for class Game

--- objectional/bowling_scorer.py
+++ objectional/bowling_scorer.py
@@ -4,23 +4,44 @@
 ## Comments marked with double ## are asides to my reviewers
 
 class Game():
+  ''' Stores integer list of bowling pins knocked down per
+      roll and calculates game score.
 
-  _scores = []
-  _total_score
+      Atributes: list _scores, int _total_score, bool _scored '''
 
   def __init__(self,raw_scores):
-    set_score(raw_scores)
+    set_raw_scores(raw_scores)
 
-## getters and setters are not idiomatic in python, but arrays
-## are passed by referrence so this avoids confusion
+## Getters and setters are not idiomatic in python, but
+## we need an isolated copy of the integer list and
+## should not rely on the caller to provide it
 
-  def set_score(raw_scores):
+## int() will raise a VaueError on a non-numeric score
+
+  def set_raw_scores(raw_scores):
+    ''' Parameters: list raw_score
+        Throws: IndexError, ValueError '''
+    # data validation
+    if len(raw_scores) > 21:
+      raise IndexError
     for score in raw_scores:
-      _scores.append(int(score))
+      # data validation
+      s = int(score)
+      if s < 0 or s > 10:
+        raise ValueError
+      # store data
+      self._scores.append(s)
+    # mark total outdated
+    self._scored = False
 
-  def get_scores(self):
+  def get_raw_scores(self):
     return _scores
 
   def score(self):
+    # don't recalc if total is up-to-date
+    if self._scored == True:
+      return _total_score
+    # make sure that _scores is not null
     # score code goes here
+    self._scored = True
     return 1
