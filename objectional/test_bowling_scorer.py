e2c5de6591b0a1b79d12925de0e0bdb7254e4a63
seamus <house.alabama@gmail.com>
2017-04-22 10:56:41 -0400

    Skeleton test cases

--- objectional/test_bowling_scorer.py
+++ objectional/test_bowling_scorer.py
@@ -4,28 +4,67 @@ import bowling_scorer
 
 ### create game ###
 
-  def test_new_game():
-    a_game = Game()
-    assert(instanceof(a_game,
+def test_new_game():
+  #a_game = Game()
+  #assert(isinstance(a_game,Game))
+  assert(1)
 
-  def test_new_game_with_too_many_entries():
+def test_new_game_with_too_many_entries():
+  assert(0)
 
-  def test_new_game_with_too_few_entries():
+def test_new_game_with_too_few_entries():
+  assert(0)
 
-  def test_new_game_with_not_integers():
+def test_new_game_with_not_integers():
+  assert(0)
 
 ### score game ###
 
-# all gutter balls
+def test_all_gutter_balls():
+  assert(0)
 
-# all strikes
+def test_all_strikes():
+  assert(0)
 
-# all spares
+def test_all_spares():
+  assert(0)
 
-# strikes followed by spares
+def test_all_open_frames():
+  assert(0)
 
-# spares followed by strikes
+def test_strikes_followed_by_spares():
+  assert(0)
 
-# all open frames
+def test_spares_followed_by_strikes():
+  assert(0)
 
-# open frames followed by strikes and spares
+def test_two_gutters_followed_by_strike():
+  assert(0)
+
+def test_spare_with_zero_ten():
+  assert(0)
+
+def test_open_frames_followed_by_strikes_and_spares():
+  assert(0)
+
+### score game - misc end games ###
+
+def test_tenth_frame_strike_gutter_strike():
+  # 10 0 10
+  assert(0)
+
+def test_tenth_frame_strike_spare():
+  # 10 5 5
+  assert(0)
+
+def test_tenth_frame_open():
+  # 5 4
+  assert(0)
+
+def test_tenth_frame_zero_spare():
+  # 0 10 2
+  assert(0)
+
+def test_tenth_frame_spare_strike():
+  # 5 5 10
+  assert(0)
