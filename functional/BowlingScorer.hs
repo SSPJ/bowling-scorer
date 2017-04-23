dc31a4e6bc74b734c9670070b1c7debc2f1a159e
seamus <house.alabama@gmail.com>
2017-04-22 22:53:41 -0400

    Make hs tests pass

--- functional/BowlingScorer.hs
+++ functional/BowlingScorer.hs
@@ -1,21 +1,20 @@
 module BowlingScorer where
---ts :: [Int]
---ts = [10,6,1,7,3,10,4,3,5,5,5,5,9,0,9,1,7,3,10]
 
 pair :: [Int] -> [(Int,Int)]
-pair [a,b]      = (a,b) : (0,0) : []
+pair [a,b]      = (a, b) : (0,0) : (0,0) : []
 pair [a,b,c]
-    | a == 10   = (10,0) : (b,c) : []
-    | otherwise = (a,b) : (c,0) : []
+    | a == 10   = (10,0) : (b,c) : (0,0) : []
+    | otherwise = (a, b) : (c,0) : (0,0) : []
 pair (10:b:xs)  = (10,0) : pair (b:xs)
-pair (a:b:xs)   = (a,b) : pair xs
+pair (a:b:xs)   = (a, b) : pair xs
 
-score :: ((Int,Int),(Int,Int)) -> Int
-score ((a,b),(c,d))
+score :: ((Int,Int),(Int,Int),(Int,Int)) -> Int
+score ((10,0),(10,0),(e,f)) = 20 + e
+score ((a,b),(c,d),(e,f))
     | a == 10   = a + c + d
     | a+b == 10 = a + b + c
     | otherwise = a + b
 
-zip_self xs = zip xs (drop 1 xs)
+zip_self xs = zip3 xs (drop 1 xs) (drop 2 xs)
 
 game xs  = sum [ score x | x <- zip_self (pair xs) ]
