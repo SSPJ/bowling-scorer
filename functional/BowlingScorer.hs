925e682e3278c9ffcd943f0d1185c0c1765432ab
seamus <house.alabama@gmail.com>
2017-04-23 12:50:32 -0400

    Refactor hs for less mem and cpu

--- functional/BowlingScorer.hs
+++ functional/BowlingScorer.hs
@@ -1,20 +1,44 @@
 module BowlingScorer where
+-- Seamus Johnston, 2017
 
+-- pair seperates the list of rolls into tuples
+-- by frame; frame 10 with 3 rolls gets two tuples
 pair :: [Int] -> [(Int,Int)]
-pair [a,b]      = (a, b) : (0,0) : (0,0) : []
+-- base cases: frame ten
+pair [a,b]      = (a, b) : (0,0) : []
 pair [a,b,c]
-    | a == 10   = (10,0) : (b,c) : (0,0) : []
-    | otherwise = (a, b) : (c,0) : (0,0) : []
-pair (10:b:xs)  = (10,0) : pair (b:xs)
-pair (a:b:xs)   = (a, b) : pair xs
+    | a == 10   = (10,0) : (b,c) : []
+    | otherwise = (a, b) : (c,0) : []
+-- edge case: a strike
+pair (10:b:xs)  = (10,0)         : pair (b:xs)
+-- general case:
+pair (a:b:xs)   = (a, b)         : pair xs
 
-score :: ((Int,Int),(Int,Int),(Int,Int)) -> Int
-score ((10,0),(10,0),(e,f)) = 20 + e
-score ((a,b),(c,d),(e,f))
-    | a == 10   = a + c + d
-    | a+b == 10 = a + b + c
-    | otherwise = a + b
 
-zip_self xs = zip3 xs (drop 1 xs) (drop 2 xs)
+-- score traverses the list of frame tuples and
+-- returns a list of scores by frame
+score :: [(Int,Int)] -> [Int]
+-- base case: frame ten / end of list
+score [(a,b)]   = (a + b)     : []
+-- edge cases: end of game
+score [(a,b),(c,d)]
+--  strike or spare in frame ten
+    | a   == 10 = (a + c + d) : []
+    | a+b == 10 = (a + b + c) : []
+--  frame 9 mistaken for ten
+    | otherwise = (a + b)     : score [(c,d)]
+-- general case:
+score ((a,b):(c,d):xs)
+--  edge case: two strikes in a row, eg [(10,0),(10,0),(3,4)]
+    | a+c == 20 = (a + c + u) : score ((c,d):xs)
+--  a strike
+    | a   == 10 = (a + c + d) : score ((c,d):xs)
+--  a spare
+    | a+b == 10 = (a + b + c) : score ((c,d):xs)
+    | otherwise = (a + b)     : score ((c,d):xs)
+    where     u = fst $ head xs
 
-game xs  = sum [ score x | x <- zip_self (pair xs) ]
+
+-- game takes an Int list of pins knocked down
+-- returns an Int bowling score
+game xs = sum . score $ pair xs
